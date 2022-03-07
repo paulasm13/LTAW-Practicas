ceda5fad88b2f55a7e5d2ebd012ae2b464191b26
paulasm13 <paula13sepulved@hotmail.com>
2022-03-07 16:54:07 +0100

    URL

--- P1/tienda.js
+++ P1/tienda.js
@@ -1,12 +1,23 @@
 
-// Definición de modulos y puerto
+//-- Definición de modulos y puerto
 const http = require('http');
+const url = require('url');
 
 const PUERTO = 9090;
 
-// Creo el servidor
-const server = http.createServer();
+//-- Creo el servidor
+const server = http.createServer((req, res) => {
 
+  console.log('Petición recibida');
+
+  //-- Construir el objeto url con la url de la solicitud
+  let myURL = new URL(req.url, 'http://' + req.headers['host']);
+  console.log(" * URL completa: " + myURL.href);
+  console.log(" * Ruta: " + myURL.pathname);
+
+});
+
+//-- Activo el servidor
 server.listen(PUERTO);
 
-console.log("Escuchando en puerto: " + PUERTO);
+console.log("Servidor activado. Escuchando en puerto: " + PUERTO);
