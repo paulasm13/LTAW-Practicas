6b4e4eaf3fc236250c519e9af117d0eb16fb01c5
paulasm13 <paula13sepulved@hotmail.com>
2022-03-07 18:40:55 +0100

    .js OK

--- P1/tienda.js
+++ P1/tienda.js
@@ -2,13 +2,15 @@
 //-- Definición de modulos y puerto
 const http = require('http');
 const url = require('url');
+const fs = require('fs');
 
 const PUERTO = 9090;
+
 
 //-- Creo el servidor
 const server = http.createServer((req, res) => {
 
-  console.log('Petición recibida');
+  console.log('\nPetición recibida');
 
   //-- Construir el objeto url con la url de la solicitud
   let myURL = new URL(req.url, 'http://' + req.headers['host']);
@@ -16,7 +18,7 @@ const server = http.createServer((req, res) => {
     // * Pathname devuelve el nombre de ruta de una URL
   console.log(" * Ruta: " + myURL.pathname);
 
-  //-- Fichero a devolver
+  //-- Fichero del recurso
   let file = "";
 
   //-- Analizar el recurso solicitado
@@ -26,8 +28,40 @@ const server = http.createServer((req, res) => {
     file = myURL.pathname;
   }
 
-  console.log(' * Nombre del fichero: ' + file);
-  
+  type_file = file.split(".")[1];
+  file = '.' + file;
+  console.log(' * Recurso solicitado: ' + file);
+  console.log(' * Extensión del recurso solicitado: ' + type_file);
+
+  //-- Tipo de archivo
+  const mime_type = {
+    'html' : 'text/html',
+    'css'  : 'text/css',
+    'jpg'  : 'image/jpg',
+    'ico'  : 'image/x-icon'
+  };
+
+  let mime = mime_type[type_file];
+  console.log(' * Tipo de MIME: ' + mime);
+
+  //-- Lectura asincrona del fichero
+  fs.readFile(file, (err,data) => {
+    //-- Fichero no encontrado, página de error
+    if (err) {
+      // * La página de error es un MIME tipo 'html'
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      console.log('\n404: Not Found');
+      file = './error.html';
+      data = fs.readFileSync(file);
+      res.write(data);
+      res.end();
+    }else{
+      res.writeHead(200, {'Content-Type': mime});
+      console.log('\n200: OK');
+      res.write(data);
+      res.end();
+    }
+  });
 });
 
 //-- Activo el servidor
