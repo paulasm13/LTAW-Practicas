b35ad4a20100cf88cc8bde5e7069faaee9f277e1
paulasm13 <paula13sepulved@hotmail.com>
2022-04-26 19:39:58 +0200

    L5

--- P2/L5/tienda.js
+++ P2/L5/tienda.js
@@ -6,6 +6,9 @@ const fs = require('fs');
 //-- Npmbre del fichero JSON a leer
 const FICHERO_JSON = "tienda.json"
 
+//-- NOmbre del fichero JSON de salida
+const FICHERO_JSON_OUT = "tienda.json"
+
 //-- Leer el fichero JSON
 const  tienda_json = fs.readFileSync(FICHERO_JSON);
 
@@ -22,15 +25,21 @@ tienda["users"].forEach((element, index)=>{
 
 console.log("Productos en la tienda: " + tienda["products"].length);
 
-//-- Recorrer el array de productos
+//-- Recorrer el array de productos e incrementar su stock
 tienda["products"].forEach((element, index)=>{
   console.log("Producto: " + (index + 1) + ": " + element["nombre"] + " / " + element["descripcion"] + " / " + element["stock"]);
+  element["stock"] = element["stock"] + 1;
 });
 
 console.log("Pedidos pendientes: " + tienda["orders"].length);
-
 
 //-- Recorrer el array de pedidos
 tienda["orders"].forEach((element, index)=>{
   console.log("Pedidos pendientes: " + (index + 1) + ": " + element["usuario"] + " / " + element["direccion"] + " / " + element["tarjeta"] + " / " + element["productos"]);
 });
+
+//-- Convertir la variable a cadena JSON
+let myJSON = JSON.stringify(tienda);
+
+//-- Guardar cambios en el fichero destino
+fs.writeFileSync(FICHERO_JSON_OUT, myJSON);
