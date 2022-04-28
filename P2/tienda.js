4c2cc018d21bd3a1325be78f49bd8950eafe52d9
paulasm13 <paula13sepulved@hotmail.com>
2022-04-28 18:48:20 +0200

    Reconoce usuarios

--- P2/tienda.js
+++ P2/tienda.js
@@ -1,4 +1,3 @@
-
 //-- Definición de modulos y puerto
 const http = require('http');
 const url = require('url');
@@ -6,17 +5,9 @@ const fs = require('fs');
 
 const PUERTO = 9090;
 
-//-- Cargar pagina principal
-const MAIN = fs.readFileSync('main.html','utf-8');
-
-//-- Cargar la página de error
-const ERROR = fs.readFileSync('error.html', 'utf-8');
-
-//-- Cargar pagina web del formulario
-const FORMULARIO = fs.readFileSync('form-login.html','utf-8');
-
 //-- HTML de la página de respuesta
 const RESPUESTA = fs.readFileSync('form-resp.html', 'utf-8');
+
 
 //-- Cargar fichero JSON 
 const DATOS = fs.readFileSync('tienda.json','utf-8');
@@ -28,79 +19,96 @@ const server = http.createServer((req, res) => {
 
   //-- Construir el objeto url con la url de la solicitud
   let myURL = new URL(req.url, 'http://' + req.headers['host']);
-  let filename = "";
 
+  // Petición
   console.log(" * URL completa: " + myURL.href);
-  console.log(" * Método: " + req.method);
-  console.log(" * Ruta: " + myURL.pathname.split(".")[1]);
-  
+    // * Pathname devuelve el nombre de ruta de una URL
+  console.log(" * Ruta: " + myURL.pathname);
 
-  //-- Fichero del recurso
-  let data = "";
+  //-- Variables 
+  let filename = "";
+  let file = "";
+  let user_enter = "";
+  let password_enter = "";
+  let info = JSON.parse(DATOS);
+
 
   //-- Analizar el recurso solicitado
-  if (myURL.pathname == '/'){
+  if (myURL.pathname == '/') {
     filename = '/main.html';
-    data = MAIN;
 
   }else if (myURL.pathname == '/procesar'){
     filename = '/form-resp.html';
-
     // Leer los parámetros
-    let nombre = myURL.searchParams.get('nombre');
-    let apellidos = myURL.searchParams.get('apellidos');
-    console.log(" - Nombre: " + nombre);
-    console.log(" - Apellidos: " + apellidos);
-
-    //-- Reemplazar las palabras claves por su valores
-    //-- en la plantilla HTML
-    data = RESPUESTA.replace("NOMBRE", nombre);
-    data = data.replace("APELLIDOS", apellidos);
-
-    // Obtener el array de usuarios registrados
-    let info = JSON.parse(DATOS);
-    info_usuarios = info["users"]; 
-    console.log(info_usuarios);
-
-  }else if ((myURL.pathname.split(".")[1]) == 'css' || 'jpg' || 'png'){
-    filename = myURL.pathname;
-    data = filename.split("/")[1];
-    data = fs.readFileSync(data,'utf-8');
+    user_enter = myURL.searchParams.get('usuario');
+    password_enter = myURL.searchParams.get('contraseña');
+    console.log(" - Nombre: " + user_enter);
+    console.log(" - Apellidos: " + password_enter);
+    
   }else{
-    filename = '/error.html';
-    // * La página de error es un MIME tipo 'html'
-    res.writeHead(404, {'Content-Type': 'text/html'});
-    console.log('\n404: Not Found\n');
-    res.write(ERROR);
-    res.end();
+    filename = myURL.pathname;
   }
 
-  // Construir solicitud vol.2
-  type_file = filename.split(".")[1];
+  // Formalizar petición
   filename = '.' + filename;
+  file = filename.slice(2);
+  type_file = file.split(".")[1];
   console.log(' * Recurso solicitado: ' + filename);
   console.log(' * Extensión del recurso solicitado: ' + type_file);
 
-   //-- Tipo de archivo
-   const mime_type = {
+  //-- Tipo de archivo
+  const mime_type = {
     'html' : 'text/html',
     'css'  : 'text/css',
     'jpg'  : 'image/jpg',
+    'ico'  : 'image/x-icon',
     'png'  : 'image/png',
-    'json' : 'application/json',
   };
 
   let mime = mime_type[type_file];
   console.log(' * Tipo de MIME: ' + mime);
 
-  // Respuesta
-  res.writeHead(200, {'Content-Type': mime});
-  console.log('\n200: OK');
-  res.write(data);
-  res.end();
+
+  //-- Lectura asincrona del fichero
+  fs.readFile(file, (err,data) => {
+    //-- Fichero no encontrado, página de error
+    if (err) {
+      // * La página de error es un MIME tipo 'html'
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      console.log('\n404: Not Found');
+      file = './error.html';
+      data = fs.readFileSync(file);
+      res.write(data);
+      res.end();
+    }else{
+      res.writeHead(200, {'Content-Type': mime});
+      console.log('\n200: OK');
+      if (file == 'form-resp.html') {
+        //-- Reemplazar las palabras claves por su valores
+        //-- en la plantilla HTML
+        data = RESPUESTA.replace("USUARIO", user_enter);
+        data = data.replace("CONTRASEÑA", password_enter);
+
+        let html_extra = "";
+
+        //-- Recorrer el array de usuarios
+        info["users"].forEach((element, index)=>{
+            console.log("Usuarios registrados: " + (index + 1) + ": " + element["user"]);
+            if(user_enter == element["user"]){
+                html_extra = "<h2>USUARIO REGISTRADO<h2>";
+                data = data.replace("HTML_EXTRA", html_extra);
+            }
+        });
+      }else{
+        data = fs.readFileSync(file);
+      }
+      res.write(data);
+      res.end();
+    }
+  });
 });
 
 //-- Activo el servidor
 server.listen(PUERTO);
 
-console.log("Servidor activado. Escuchando en puerto: " + PUERTO);
+console.log("Servidor activado. Escuchando en puerto: " + PUERTO + '\n');
